8833af32b259bdf52335fe28efc821e96212f1b6
Tanna <tanna.la.lynn@gmail.com>
2016-10-01 09:46:06 -0700

    complete lab1

---
+++ lab1/lab1/lab1/Controllers/PersonController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using lab1.Models;
+
+namespace lab1.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+	        var p = new Person
+	        {
+		        name = "person mcpersonface",
+		        alignment = "chaotic neutral"
+	        };
+
+            return View(p);
+        }
+    }
+}
---
+++ lab1/lab1/lab1/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab1.Models
+{
+	public class Person
+	{
+		public String name { get; set; }
+		public String alignment { get; set; }
+
+	}
+}
