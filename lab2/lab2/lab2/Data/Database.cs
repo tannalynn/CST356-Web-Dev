06a1bd55faeeb0910e5cef1ce2d1a87b064cf979
Tanna <tanna.la.lynn@gmail.com>
2016-10-08 09:52:57 -0700

    lab2 done

---
+++ lab2/lab2/lab2/Data/Database.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using lab2.Models;
+
+namespace lab2.Data
+{
+	public class Database: DbContext
+	{
+		public Database() : base("DatabaseContext")
+        {
+		}
+
+		public DbSet<User> Users { get; set; }
+
+
+	}
+}
---
+++ lab2/lab2/lab2/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace lab2.Models
+{
+	public class User
+	{
+		public int ID { get; set; }
+
+
+		[Required]
+		[Display(Name = "First Name")]
+		public String firstName { get; set; }
+
+		[Required]
+		[Display(Name = "Last Name")]
+		public String lastName { get; set; }
+
+		[Required]
+		[Display(Name = "Email Address")]
+		public String email { get; set; }
+
+		[Required]
+		[Display(Name = "Alignment")]
+		public String alignment { get; set; }
+	}
+}
